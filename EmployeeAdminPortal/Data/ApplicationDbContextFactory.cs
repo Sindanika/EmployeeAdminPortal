0a7e2eaa6d61da6be5450b52de2fb4ea947208d8
HP <HP@DESKTOP-2TU77VP>
2025-05-17 21:42:43 +0530

    Add project files.

---
+++ EmployeeAdminPortal/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using EmployeeAdminPortal.Data; // ✔️ adjust if your namespace is different
+
+namespace EmployeeAdminPortal
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            // ❗ Use your actual connection string
+            optionsBuilder.UseSqlServer("Server=desktop-2tu77vp;Database=EmployeeDb;Trusted_Connection=True;TrustServerCertificate=True;");
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ EmployeeAdminPortal/Models/UpdateEmployeeDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeAdminPortal.Models
+{
+    public class UpdateEmployeeDto
+    {
+       
+            public string Name { get; set; }
+
+            [EmailAddress]
+            public string Email { get; set; }
+
+            public string Phone { get; set; }
+
+            public string Salary { get; set; }
+        
+
+    }
+}
